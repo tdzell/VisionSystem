6d7141fe17795788f56cd512afacb7127efee8b9
Tyler Zell <chi2006bears@yahoo.com>
2017-11-22 14:32:02 -0600

    Added multiple process support for Standard cameras

--- liveVideoDetect.py
+++ liveVideoDetect.py
@@ -6,6 +6,8 @@ import AlarmDetector
 from pyueye_example_camera import Camera
 from pyueye_example_utils import *
 import numpy as np
+import multiprocessing
+from multiprocessing import Queue, Pool
 from threading import Thread
 from ctypes import byref
 import sharing
@@ -16,7 +18,7 @@ def IDSCamera(cfgfile, weightfile, useGPU):
     
     ### initialization for creation of a .avi file for sharing of proof of concept
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    out = cv2.VideoWriter('output.avi',fourcc,25.0,(640,480))
+    out = cv2.VideoWriter('output.avi',fourcc,4.0,(640,480))
 
     
     ### IDS camera initializations
@@ -43,27 +45,7 @@ def IDSCamera(cfgfile, weightfile, useGPU):
     
     
 def StandardCamera(cfgfile, weightfile, useGPU):
-    
-    ### initialization of neural network based upon the specified config and weights files
     m = Darknet(cfgfile)
-    m.print_network()
-    m.load_weights(weightfile)
-    
-        
-    if m.num_classes == 20:
-            namesfile = 'data/voc.names'
-    elif m.num_classes == 80:
-            namesfile = 'data/coco.names'
-    else:
-            namesfile = 'data/names'
-
-    class_names = load_class_names(namesfile)
-    
-    ### if GPU optimizations are enabled, do some initialization
-    if sharing.usegpu:
-        m.cuda()
-    print('Loading weights from %s... Done!' % (weightfile))
-    
     ### initialization for creation of a .avi file for sharing of proof of concept
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     out = cv2.VideoWriter('output.avi',fourcc,4.0,(640,480))
@@ -76,34 +58,101 @@ def StandardCamera(cfgfile, weightfile, useGPU):
     if not cap.isOpened():
         print("Unable to open camera")
         exit(-1)
+####################
+    num_workers = 2
+    input_q = Queue(4)
+    output_q = Queue(4)
+    res, img = cap.read()
+            
+    input_q.put(img)  
+    pool = Pool(num_workers, Standard_worker, (input_q, output_q, cfgfile, weightfile, useGPU))
+        
+    if m.num_classes == 20:
+            namesfile = 'data/voc.names'
+    elif m.num_classes == 80:
+            namesfile = 'data/coco.names'
+    else:
+            namesfile = 'data/names'
 
+    class_names = load_class_names(namesfile)    
+        
+    for x in range(200):
+        
+        res, img = cap.read()
+            
+        input_q.put(img)
+        
+        img, bboxes = output_q.get()
+        print('------')
+        draw_img, waitsignal = plot_boxes_cv2(img, bboxes, None, class_names) #draw boxes associated with detections onto the base images | AlarmDetection.py is called in here
+        cv2.imshow('cfgfile', draw_img) #show the image frame that now has detections drawn onto it | draw_image will be entirely green/yellow/red after a judgement is made by AlarmDetection.py for verification or alarm
+        
+        '''uncomment the following line to record video | file is named output.avi and will overwrite any existing files with same name'''        
+        #out.write(draw_img)
+        
+        if waitsignal == True: #if green/yellow/red screen is being shown by draw_img, leave it in place for two seconds instead of continuing detections
+            cv2.waitKey(2000)
+            waitsignal = False
+    
+        cv2.waitKey(3) #neccessary to ensure this loop does not attempt to pull new images from the USB camera too quickly
+        
+            
+        
+  #      if cv2.waitKey(1) & 0xFF == ord('q'):
+  #          break
+        
+    pool.terminate()
+    cap.stop()
+    cv2.destroyAllWindows()
+        
+        
+#####################        
+    
+def Standard_worker(input_q, output_q, cfgfile, weightfile, useGPU):
+    
+    
+    sharing.detect_min = 3
+    sharing.colorframe = 'nothing'
+    sharing.saveimage  = False
+    sharing.counterimage = 0
+    
+    if useGPU:
+        sharing.usegpu = True
+    else:
+        sharing.usegpu = False
+    
+    ### initialization of neural network based upon the specified config and weights files
+    m = Darknet(cfgfile)
+    m.print_network()
+    m.load_weights(weightfile)
+    
+        
+    
+    ### if GPU optimizations are enabled, do some initialization
+    if sharing.usegpu:
+        m.cuda()
+    print('Loading weights from %s... Done!' % (weightfile))
+    
     while True:
 
         
-        res, img = cap.read()
+        img = input_q.get()
         
-        
-        if res: #res is only true if an image frame was successfully read the the generic USB camera
-            sized = cv2.resize(img, (m.width, m.height)) #resize the image frame pulled into the size expecteed by the detection model
-            bboxes = do_detect(m, sized, 0.4, 0.4, useGPU) #third value in this call sets the confidence needed to detect object
-            print('------')
-            draw_img, waitsignal = plot_boxes_cv2(img, bboxes, None, class_names) #draw boxes associated with detections onto the base images | AlarmDetection.py is called in here
-            cv2.imshow('cfgfile', draw_img) #show the image frame that now has detections drawn onto it | draw_image will be entirely green/yellow/red after a judgement is made by AlarmDetection.py for verification or alarm
-            out.write(draw_img)
-            if waitsignal == True: #if green/yellow/red screen is being shown by draw_img, leave it in place for two seconds instead of continuing detections
-                cv2.waitKey(2000)
-                waitsignal = False
-
-            '''uncomment the following line to record video | file is named output.avi and will overwrite any existing files with same name'''
-            #out.write(draw_img) 
+        sized = cv2.resize(img, (m.width, m.height)) #resize the image frame pulled into the size expecteed by the detection model
+        #bboxes = do_detect(m, sized, 0.4, 0.4, useGPU) #third value in this call sets the confidence needed to detect object
             
-            cv2.waitKey(1) #neccessary to ensure this loop does not attempt to pull new images from the USB camera too quickly
-        else:
-             print("Unable to read image")
-             exit(-1)
+        output_q.put((img, do_detect(m, sized, 0.4, 0.4, useGPU)))
+            
 
+    #################################################
+    
+        
     
     
+    
+    
+    
+  ###########################################################  
 class FrameThread(Thread):
     def __init__(self, cam, views, cfgfile, weightfile, useGPU, copy=True):
         super(FrameThread, self).__init__()
@@ -173,7 +222,7 @@ class FrameThread(Thread):
                 if waitsignal == True:
                     cv2.waitKey(2000)
                     waitsignal = False
-                cv2.waitKey(150)
+                cv2.waitKey(100)
               
                 
 ############################################
