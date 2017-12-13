c08024fb7a65937f7520aa8403262b1097cd6e73
Tyler Zell <chi2006bears@yahoo.com>
2017-12-13 10:35:30 -0600

    GPIO Pin test

--- AlarmDetector.py
+++ AlarmDetector.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2
 import sharing
+import RPI.GPIO as GPIO
+
+def SoundAlarm():
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
+GPIO.output(14, 1)    
+cv2.waitKey(250)
+GPIO.output(14, 0)
+GPIO.cleanup()
 
 def GlobeCreate(): ###initializes all global variables that need to be tracked in this module
 
@@ -68,91 +78,6 @@ def AlarmDetect(DetectedClasses, ClassNames, imgToBeSaved):
     outerCount = Counter[1][2]
     handleCount = Counter[1][1] 
     
-    if boltCount == 0 :
-        noBoltSeen += 1 # if no bolts have been seen, increment this counter | if a judgement needs to occur, it will only happen once no bolts have been seen for X frames 
-    if boltCount == 1:
-        oneBoltSeen += 1 #increment this number of bolts seen
-        noBoltSeen = 0 #reset the reset counter | if a judgement needs to occur, it will only happen once no bolts have been seen for X frames
-    if boltCount == 2:
-        twoBoltSeen += 1
-        noBoltSeen = 0
-    if boltCount == 3:
-        threeBoltSeen += 1
-        noBoltSeen = 0
-    if boltCount == 4:
-        fourBoltSeen += 1
-        noBoltSeen = 0
-    if boltCount > 4: #for the current application, no more than 4 bolts should ever possibly be seen at one time
-        print('ERROR: MORE THAN 4 BOLTS SEEN')
-    
-    if outerCount == 1 : 
-        oneOuterSeen += 1 
-        noBoltSeen = 0 #reset the reset counter | if a judgement needs to occur, it will only happen once no bolts have been seen for X frames
-    elif outerCount == 2:
-        twoOuterSeen += 1
-        noBoltSeen = 0
-    if outerCount > 2:
-        print('ERROR: MORE THAN 2 OUTER SEEN')
-
-    if handleCount == 1:
-        handleSeen += 1
-        noBoltSeen = 0
+    if boltCount > 0 :
+        SoundAlarm()
         
-    if handleCount > 1:
-        print('ERROR: MORE THAN 1 HANDLE SEEN')
-    print('no bolt: %s' % (noBoltSeen))
-    colorframe = 'nothing'
-    
-    if (oneBoltSeen + twoBoltSeen + threeBoltSeen + fourBoltSeen) == 1:
-        
-        sharing.holdimg = imgToBeSaved
-    
-    
-    if noBoltSeen > 8: #if no bolts have been seen for X frames, check if alarm needs to be raised, otherwise return to the calling script
-        boltExpected = 0
-    
-        if twoOuterSeen >= sharing.detect_min:
-            boltExpected += 2 #if two "outer signs" have been detected, there should be two associated bolts that were also seen
-        elif oneOuterSeen >= sharing.detect_min:
-            boltExpected += 1 #if one "outer sign" has been detected, there should be one associated bolt that was also seen
-        
-        if handleSeen >= sharing.detect_min:
-            boltExpected += 2 #if a "handle" has been detected, than there should be two associated bolts that were also seen
-    
-        if fourBoltSeen >= sharing.detect_min: #if four bolts were seen at once for at least 'sharing.detect_min' times, then we assume that four bolts were seen
-            boltSeen = 4
-        elif threeBoltSeen >= sharing.detect_min: #etc for three bolts
-            boltSeen = 3
-        elif twoBoltSeen >= sharing.detect_min: #etc for two bolts
-            boltSeen = 2
-        elif oneBoltSeen >= sharing.detect_min: #etc for one bolt
-            boltSeen = 1
-        else:
-            boltSeen = 0 #else no bolts were seen
-        
-        if boltExpected != 0:
-            if boltSeen > boltExpected:
-                print('||CONFUSED:, %s bolts seen but %s expected' % (boltSeen, boltExpected))
-                sharing.saveimage = True #mark that the held image should be saved to file by the calling script
-                sharing.colorframe = 'yellow' #mark that a yellow screen should be returned instead of normal detection feed
-            if boltSeen == boltExpected:
-                print('VERIFIED, %s bolts expected' % (boltExpected))
-                sharing.colorframe = 'green' #mark that a green screen should be returned instead of normal detection feed
-            if boltSeen < boltExpected:
-                print('**ALARM**, %s bolts seen but %s expected' % (boltSeen, boltExpected))
-                sharing.saveimage = True #mark that the held image should be saved to file by the calling script
-                sharing.colorframe = 'red' #mark that a red screen should be returned instead of normal detection feed
-            #cv2.waitKey(0)
-        else:
-            sharing.colorframe = 'nothing'
-            
-        noBoltSeen = 0
-        oneBoltSeen = 0
-        twoBoltSeen = 0
-        threeBoltSeen = 0
-        fourBoltSeen = 0
-        oneOuterSeen = 0
-        twoOuterSeen = 0
-        handleSeen = 0
-        
-    return colorframe, saveimage
