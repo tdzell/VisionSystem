740b52a3d1dd76c1a6cf443012ce8400b5645b03
Tyler Zell <chi2006bears@yahoo.com>
2017-12-14 13:16:44 -0600

    Remove class id gating

--- utils.py
+++ utils.py
@@ -198,8 +198,6 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
         if len(box) >= 7 and class_names:
             cls_conf = box[5]
             cls_id = box[6]
-            if cls_id > 6:
-                cls_id = 6
             print('%s: %f' % (class_names[cls_id], cls_conf))
             classes = len(class_names)
             offset = cls_id * 123457 % classes
